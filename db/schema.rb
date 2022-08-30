2ba172ce95956d3a40ae2131a41ad95abf89c1be
Tim Koar <koartimothy@gmail.com>
2022-08-30 12:23:49 -0400

    Created User model with validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_25_190904) do
-  create_table "customers", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_150923) do
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "ccNum"
-    t.string "ccexpDate"
-    t.string "ccCvv"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
